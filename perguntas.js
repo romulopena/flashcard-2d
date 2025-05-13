d64348d74d6899a3e5c684b9af8c14ddf2cce446
PROF ROMULO <rjlpena@gmail.com>
2025-05-13 14:27:59 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'Linguagem de marcação'
+)
+
+criaCartao(
+    'Inglês',
+    'Como é dia do pagamento?',
+    'Pay Day'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
